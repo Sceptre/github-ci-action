c78773f57625fd55ff936f830f31b6aa026e809a
Bruno Grande <bruno.grande@sagebase.org>
2021-07-01 17:46:57 -0700

    Make Pipfile method mutually exclusive

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,26 @@ function parseInputs {
   if [[ "${INPUT_SCEPTRE_SUBCOMMAND}" != "" ]]; then
     sceptreSubcommand=${INPUT_SCEPTRE_SUBCOMMAND}
   else
-    echo "Input sceptre_subcommand cannot be empty!"
+    echo "ERROR: Input sceptre_subcommand cannot be empty!"
     exit 1
   fi
 
   # Optional inputs
-  sceptreVer='2.5.0'
   if [[ "${INPUT_SCEPTRE_VERSION}" != "" ]]; then
     if [[ "${INPUT_SCEPTRE_VERSION}" =~ ^2[.][0-9][.][0-9]$ ]]; then
       sceptreVer=${INPUT_SCEPTRE_VERSION}
     else
-      echo "Unsupported sceptre version"
+      echo "ERROR: Unsupported sceptre version"
       exit 1
     fi
   else
-    echo "Input sceptre_version cannot be empty!"
-    exit 1
+    if [[ "${INPUT_SCEPTRE_PIPFILE}" == "" && "${INPUT_SCEPTRE_REQUIREMENTS}" == "" ]]; then
+      echo "ERROR: Input sceptre_version cannot be empty!"
+      exit 1
+    else
+      echo "WARNING: Sceptre version not specified, so it's assumed that it"
+      echo "-------  is specified in the Pipfile or requirements file."
+    fi
   fi
 
   sceptreDir="."
@@ -62,36 +66,48 @@ function parseInputs {
     echo "-------  Consider only specifying Python dependencies in one or the other"
   fi
 
+  if [[ "${INPUT_SCEPTRE_PIPFILE}" != "" ]] \
+     && [[   "${INPUT_SCEPTRE_REQUIREMENTS}" != "" \
+          || "${INPUT_SCEPTRE_PLUGINS}" != "" \
+          || "${INPUT_SCEPTRE_TROPOSPHERE_VERSION}" != "" \
+          || "${INPUT_SCEPTRE_VERSION}" != "" ]]; then
+        echo "WARNING: Detected Pipfile and at least one other method for specifying"
+        echo "-------  dependencies/versions. Only the Pipfile will be installed."
+        echo "-------  Consider only specifying the dependencies/versions in the Pipfile."
+    fi
+  fi
+
   if [[ "${INPUT_SCEPTRE_PIPFILE}" != "" || "${INPUT_SCEPTRE_REQUIREMENTS}" != "" ]]; then
 
     if [[ "${INPUT_SCEPTRE_PLUGINS}" != "" ]]; then
-        echo "WARNING: Detected Pipfile and/or requirements file and Sceptre plugins"
-        echo "-------  Consider only specifying the Sceptre plugins in either file"
+        echo "WARNING: Detected Pipfile and/or requirements file and Sceptre plugins."
+        echo "-------  Consider only specifying the Sceptre plugins in either file."
     fi
 
-    if [[ "${sceptreTroposphere}" == 1 ]]; then
-      echo "WARNING: Detected Pipfile and/or requirements file and Troposhere version"
-      echo "-------  Consider only specifying the Troposphere version in either file"
+    if [[ "${INPUT_SCEPTRE_TROPOSPHERE_VERSION}" != "" ]]; then
+      echo "WARNING: Detected Pipfile and/or requirements file and Troposhere version."
+      echo "-------  Consider only specifying the Troposphere version in either file."
     fi
 
     if [[ "${INPUT_SCEPTRE_VERSION}" != "" ]]; then
-      echo "WARNING: Detected Pipfile and/or requirements file and a Sceptre version"
-      echo "-------  Consider only specifying the Sceptre version in the Pipfile"
+      echo "WARNING: Detected Pipfile and/or requirements file and a Sceptre version."
+      echo "-------  Consider only specifying the Sceptre version in the Pipfile."
     fi
 
   fi
 }
 
 function installDeps {
-  cd ${GITHUB_WORKSPACE}
 
   if [[ "${INPUT_SCEPTRE_PIPFILE}" != "" ]]; then
     echo "Installing Pipenv"
-    pip install pipenv
+    if [[ "${INPUT_SCEPTRE_PIPENV_VERSION}" != "" ]]; then
+      pip install --no-input pipenv==$INPUT_SCEPTRE_PIPENV_VERSION
+    else
+      pip install --no-input pipenv
+    fi
     echo "Installing Python Pipfile"
-    cd $(dirname ${GITHUB_WORKSPACE}/${INPUT_SCEPTRE_PIPFILE}) && pipenv install
-    pipenv shell
-    cd ${GITHUB_WORKSPACE}
+    (cd $(dirname ${GITHUB_WORKSPACE}/${INPUT_SCEPTRE_PIPFILE}) && pipenv install)
   fi
 
   if [[ "${INPUT_SCEPTRE_REQUIREMENTS}" != "" ]]; then
@@ -104,12 +120,12 @@ function installDeps {
     pip install --no-input ${INPUT_SCEPTRE_PLUGINS}
   fi
 
-  if [[ "${sceptreTroposphere}" == 1 ]]; then
+  if [[ "${INPUT_SCEPTRE_TROPOSPHERE_VERSION}" != "" ]]; then
     echo "Installing Troposphere version $sceptreTroposphereVer"
     pip install --no-input troposphere==$sceptreTroposphereVer
   fi
 
-  if [[ "${INPUT_SCEPTRE_VERSION}" != "" ]] || ! command -v sceptre &> /dev/null; then
+  if [[ "${INPUT_SCEPTRE_VERSION}" != "" ]]; then
     echo "Installing Sceptre version $sceptreVer"
     pip install --no-input sceptre==$sceptreVer
   fi
@@ -121,7 +137,12 @@ function main {
   installDeps
 
   cd ${GITHUB_WORKSPACE}/${sceptreDir}
-  sceptre $sceptreSubcommand
+
+  if [[ "${INPUT_SCEPTRE_PIPFILE}" != "" ]]; then
+    pipenv run sceptre $sceptreSubcommand
+  else
+    sceptre $sceptreSubcommand
+  fi
 }
 
 main "${*}"
